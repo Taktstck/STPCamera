5630349f77d984c57885cf400a392b88e48ed64c
1_am_a_geek <tmy0x3@icloud.com>
2015-11-06 16:26:37 +0900

    add STPCamera.h

---
+++ STPCamera/STPCamera.h
@@ -0,0 +1,15 @@
+//
+//  STPCamera.h
+//  STPCamera
+//
+//  Created by 1amageek on 2015/11/06.
+//  Copyright © 2015年 Stamp inc. All rights reserved.
+//
+
+#ifndef STPCamera_h
+#define STPCamera_h
+
+#import "STPCameraManager.h"
+#import "STPCameraView.h"
+
+#endif /* STPCamera_h */
